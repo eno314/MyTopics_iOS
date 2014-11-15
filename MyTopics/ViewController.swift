ca7a72b4074c7ee3ea1c97efd7ec236d27390f5a
Hiroto Kitamura <kirikonknow@gmail.com>
2014-11-15 14:08:26 +0900

    Initial Commit

---
+++ MyTopics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyTopics
+//
+//  Created by hiroto kitamur on 2014/11/15.
+//  Copyright (c) 2014年 eno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
